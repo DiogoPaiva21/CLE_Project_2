869a0719215abc4901b4f2662f22ab9edc9268f6
Diogo Paiva <diogopaiva21@ua.pt>
2024-05-03 15:35:19 +0100

    Times added

--- run.sh
+++ run.sh
@@ -4,4 +4,6 @@
 mpicc -Wall -O3 -o out main.c reader/fileReader.c sort/bitonicSort.c -lm
 
 # Run
-mpiexec -n 8 ./out files/datSeq16M.bin
+for (( i = 0; i < 5; i++ )); do
+  mpiexec -n 8 ./out files/datSeq16M.bin
+done
